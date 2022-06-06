749d1a99f9d11f71968dc01d8990f3e764d466bb
Sugam Jaiswal <67289887+jaissugam@users.noreply.github.com>
2022-06-07 00:12:18 +0545

    checked flask routing

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app=Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def home():
     return render_template('home.html')
+
+@app.route('/recom')
+def recommend():
+    book=request.args['book']
+    return book
