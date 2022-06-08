4cf602113afdcb233aba3ff83a371a74844e5a53
Sugam Jaiswal <67289887+jaissugam@users.noreply.github.com>
2022-06-09 00:23:45 +0545

    added error handling

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,flash
+from flask import Flask,render_template,request,flash,abort
 from recom import recommend
 
 app=Flask(__name__)
@@ -9,7 +9,14 @@ def home():
     if request.method=='POST':
         book=request.form['book']
         recoms=recommend(book)
-        flash('You might enjoy reading the following books...')
-        return render_template('home.html',recoms=recoms)
+        if len(recoms)>0:
+            flash('You might enjoy reading the following books...')
+            return render_template('home.html',recoms=recoms)
+        else:
+            return abort(404)
     else:
         return render_template('home.html')
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    return render_template('page-not-found.html'),404
--- recom.py
+++ recom.py
@@ -2,11 +2,12 @@ from preprocess import *
 
 def recommend(book):
     books,proximityVector=finalData()
-    index = books[books['title'] == book].index[0]
-    distances = sorted(list(enumerate(proximityVector[index])),reverse=True,key = lambda x: x[1])
     recomms=[]
-    for i in distances[1:9]:
-        recomms.append(books.iloc[i[0]].title)
+    if book in books['title'].tolist():
+        index = books[books['title'] == book].index[0]
+        distances = sorted(list(enumerate(proximityVector[index])),reverse=True,key = lambda x: x[1])
+        for i in distances[1:6]:
+            recomms.append(books.iloc[i[0]].title)
     return recomms
 
 #choice=input("Enter a book you like : ")
