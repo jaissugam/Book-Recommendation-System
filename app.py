d6b5760f932eb1b127e3d1de2acb8ee67e79c7e6
Sugam Jaiswal <67289887+jaissugam@users.noreply.github.com>
2022-06-08 19:16:20 +0545

    made single page

--- app.py
+++ app.py
@@ -5,10 +5,9 @@ app=Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def home():
-    return render_template('home.html')
-
-@app.route('/recom',methods=['GET','POST'])
-def recom():
-    book=request.form['book']
-    recoms=recommend(book)
-    return (" | ".join(recoms))
+    if request.method=='POST':
+        book=request.form['book']
+        recoms=recommend(book)
+        return render_template('home.html',recoms=recoms)
+    else:
+        return render_template('home.html')
