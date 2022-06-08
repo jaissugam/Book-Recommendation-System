32a984bd64d7d3ea1027edeb9ea686e47ac6e6e8
Sugam Jaiswal <67289887+jaissugam@users.noreply.github.com>
2022-06-08 20:24:41 +0545

    implemented flash

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,flash
 from recom import recommend
 
 app=Flask(__name__)
+app.secret_key = 'a#xtQ1$%op'
 
 @app.route('/',methods=['GET','POST'])
 def home():
     if request.method=='POST':
+        flash('Hold on..Fetching your next reads!')
         book=request.form['book']
         recoms=recommend(book)
+        flash('You might enjoy reading the following books...')
         return render_template('home.html',recoms=recoms)
     else:
         return render_template('home.html')
