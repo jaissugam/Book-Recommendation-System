d4137790f5ff68b624185043382012927314a7ae
Sugam Jaiswal <67289887+jaissugam@users.noreply.github.com>
2022-06-16 00:16:32 +0545

    fixed columns

--- recom.py
+++ recom.py
@@ -6,7 +6,7 @@ def recommend(book):
     if book in books['title'].tolist():
         index = books[books['title'] == book].index[0]
         distances = sorted(list(enumerate(proximityVector[index])),reverse=True,key = lambda x: x[1])
-        for i in distances[1:6]:
+        for i in distances[1:7]:
             temp=[]
             bookTitle=books.iloc[i[0]].title
             bookThumbnail=getThumbnail(bookTitle)
