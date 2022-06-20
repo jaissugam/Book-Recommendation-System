f69e87427c34b79b79f1e54c3c966eca2a1f812f
Sugam Jaiswal <67289887+jaissugam@users.noreply.github.com>
2022-06-20 23:49:24 +0545

    added gzip

--- deploy/preprocess.py
+++ deploy/preprocess.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import pickle
 from sklearn.metrics.pairwise import cosine_similarity
+import gzip
 
 def findCosine(v):
     proximityVector=cosine_similarity(v)
@@ -9,7 +10,7 @@ def findCosine(v):
 
 def finalData():
     books=pickle.load(open("new-books-list.pkl","rb"))
-    vect=pickle.load(open("count-vector.pkl","rb"))
+    vect=pickle.load(gzip.open("count-vector.zip","rb"))
     proximityVector=findCosine(vect)
     return books,proximityVector
 
@@ -22,3 +23,13 @@ def getThumbnail(title):
     new=pickle.load(open("new-books-list.pkl","rb"))
     id=new.loc[new['title'] == title, 'isbn13'].iloc[0]
     return old.loc[old['isbn13']==id,'thumbnail'].iloc[0]
+
+
+#def zipVector(books):
+#    vect=CountVectorizer(max_features=4000,stop_words='english')
+#    vector=vect.fit_transform(books['label']).toarray()
+    #return vector
+    #pickle.dump(vector,open("count-vector.pkl","wb"))
+#    pickle.dump(vector, gzip.open("count-vector.zip","wb"))
+
+#zipVector(pickle.load(open("new-books-list.pkl","rb")))
